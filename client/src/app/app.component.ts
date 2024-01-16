d5ff46cdef73bf9fc1638ab14521021a6fbd3f77
AndressaSRodrigues <srandressa2@gmail.com>
2024-01-15 21:09:20 -0300

    Added tree location with google maps

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    GoogleMapsModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
 
 export class AppComponent {
